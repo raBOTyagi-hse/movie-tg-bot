ef964dcec594cf98fad1008330ff8ca9d6fdeb73
NickVeld <nickveld@users.noreply.github.com>
2018-12-09 18:35:45 +0300

    db_shell config reading changed and 2 new SQL, 2 old edited

--- core/services/db_shell.py
+++ core/services/db_shell.py
@@ -11,12 +11,17 @@ class DBShell:
 
 
     def get_from_config(self, cfg):
-        self.db = (mysql.connector.connect(
+        self.connection = (mysql.connector.connect(
             host=cfg["db_settings"]["host"],
             port=cfg["db_settings"]["port"],
             user=cfg["db_settings"]["user"],
-            passwd=cfg["db_settings"]["passwd"]
-        ) if cfg['db_settings']['isEnabled'] == 'True' else None).cursor()
+            passwd=cfg["db_settings"]["passwd"],
+            database=cfg["db_settings"]["db_name"],
+            autocommit=True
+        ) if cfg['db_settings']['is_enabled'] == 'True' else None)
+
+        if not self.connection is None:
+            self.db = self.connection.cursor()
 
         self.INACT_M = int(cfg["user_inactivity_time_at_minutes"])
 
@@ -28,22 +33,35 @@ class DBShell:
 
     def update_last_activity(self, pers_id, after_quit):
         if not self.db is None:
-            self.db.execute("UPDATE Users SET last_activity = {} WHERE userId = {};".format(
+            self.db.execute("UPDATE Users SET uLast_activity = {} WHERE uId = {};".format(
                                 datetime.datetime.min if after_quit else datetime.datetime.utcnow(), pers_id))
 
 
     def get_ready_for_autoquit(self):
-        return self.db.execute("SELECT userId FROM Users WHERE last_activity > {} AND last_activity < {};".format(
-                                datetime.datetime.min, datetime.datetime.utcnow() - datetime.timedelta(minutes=(self.INACT_M-1))))
+        self.db.execute("SELECT uId FROM Users WHERE uLast_activity > {} AND uLast_activity < {};".format(
+                         datetime.datetime.min, datetime.datetime.utcnow() - datetime.timedelta(minutes=(self.INACT_M-1))))
+        return list(map(lambda x: x[0], self.db))
+
+    def get_movies_of_character(self, character_name):
+        self.db.execute("SELECT mTitle FROM Characters JOIN Movies ON chName = '{}' AND mId = movieId;".format(
+                         character_name))
+        return list(map(lambda x: x[0], self.db))
 
     def get_characters_of_movie(self, movie):
-        request = "SELECT DISTINCT chName FROM Characters"
+        request = "SELECT DISTINCT chName FROM Characters "
         if isinstance(movie, int):
-            request += "WHERE mId = " + str(movie)
+            request += "WHERE movieId = " + str(movie) + ";"
         else:
-            request += "JOIN Movies ON mName = {} AND mId = movieId".format(movie)
-        return self.db.execute(request)
+            request += "JOIN Movies ON mTitle = '{}' AND mId = movieId;".format(movie)
+        self.db.execute(request)
+        return list(map(lambda x: x[0], self.db))
 
-    def get_movie_of_character(self, character_name):
-        return self.db.execute("SELECT mName FROM Characters JOIN Movies ON chName = {} AND mId = movieId".format(
-                                character_name))
+    def get_most_searched_films(self):
+        self.db.execute('''SELECT M.mTitle, COUNT(Q.movie_usedId)
+FROM Movies AS M
+JOIN Queries AS Q ON M.mId = Q.movie_usedId
+GROUP BY M.mTitle, Q.movie_usedId
+ORDER BY COUNT(Q.movie_usedId); ''')
+        return list(self.db)
+
+
