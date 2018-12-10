c790cadeb185d92633159ff480207b113c9aaba2
NickVeld <nickveld@users.noreply.github.com>
2018-12-11 00:07:19 +0300

    workers [not tested] new buttons, waitlist fix,  add_user_history not done

--- core/workers/workers.py
+++ core/workers/workers.py
@@ -46,17 +46,23 @@ class BaseWorker(object, metaclass=WorkersList):
               'text': "Получить фильм(ы), из которого(ых) введенный персонаж", }],
             [{'callback_data': Encyclopedia.COMMAND + '2',
               #'text': "Get all characters of the entered movie",
-              'ltext': "Получить список персонажей введенного фильма", }],
+              'text': "Получить список персонажей введенного фильма", }],
             [{'callback_data': Encyclopedia.COMMAND + '3',
-              'text': "Получить самые разыскиваемые фильмы"}],
+              'text': "Получить самые популярные для поиска фильмы"}],
             [{'callback_data': Encyclopedia.COMMAND + '4',
               'text': "Получить информацию о распределении по годам производства фильмов"}],
             [{'callback_data': Encyclopedia.COMMAND + '5',
               'text': "Получить информацию о количестве фильмов произведенных в один год с похожим рейтингом"}],
-            # [{'callback_data': Encyclopedia.COMMAND + '6',
-            #  'text': "Получить список найденных мною фильмов"}],
+            [{'callback_data': Encyclopedia.COMMAND + '6',
+              'text': "Получить мою историю запросов"}],
             # [{'callback_data': Encyclopedia.COMMAND + '7',
             #  'text': "Найти фильм/персонажей по приблизительной цитате"}],
+            [{'callback_data': Changer.COMMAND + '1',
+              'text': "Сделать запрос на удаление персональных данных!", }],
+            [{'callback_data': Changer.COMMAND + '2',
+              'text': "Удалить фильм, так как я являюсь владельцев авторских прав!", }],
+            [{'callback_data': Changer.COMMAND + '3',
+              'text': "Удалить персонажа, так как он террорист или того хуже притесняет меньшинства!"}],
             [{'callback_data': Info.COMMAND,
               'text': "Помощь/дополнительная информация"}]
         ]
@@ -122,7 +128,7 @@ class Encyclopedia(BaseWorker):
 
     def run(self, tmsg):
         if (tmsg.pers_id == tmsg.chat_id):
-            self.tAPI.db_shell.modify_last_activity(tmsg.pers_id, False)
+            self.tAPI.db_shell.update_last_activity(tmsg.pers_id, False)
         if (tmsg.pers_id, tmsg.chat_id) in self.waitlist:
             query = tmsg.text
             command = self.waitlist[(tmsg.pers_id, tmsg.chat_id)]
@@ -130,47 +136,119 @@ class Encyclopedia(BaseWorker):
             command = tmsg.text.split()
             query = " ".join(command[1:])
             command = command[0]
-        if command.endswith(Encyclopedia.COMMAND):
+        if command.endswith(self.COMMAND):
             self.tAPI.send_inline_keyboard("Меню энциклопедии", tmsg.chat_id,
-                                           [x for x in self.MENU_KEYBOARD if x['callback_data'].startswith(Encyclopedia.COMMAND)],
+                                           [x for x in self.MENU_KEYBOARD if x['callback_data'].startswith(self.COMMAND)],
                                            tmsg.id)
         elif command.endswith('1'):
-            if len(command) > 1:
+            if len(query) > 1:
                 ms = "Персонаж играет в:\n"  # "The character plays in:\n"
                 self.tAPI.send_inline_keyboard(ms + ("\n".join(self.tAPI.db_shell.get_movies_of_character(query.upper()))),
                                tmsg.chat_id, self.MENU_KEYBOARD, tmsg.id)
+                #self.tAPI.db_shell.add_user_history(tmsg.pers_id, command + " " + query, 55)
             else:
-                self.waitlist[(tmsg.pers_id, tmsg.chat_id)] = '1'
+                self.waitlist[(tmsg.pers_id, tmsg.chat_id)] = self.COMMAND + '1'
                 ms = "Введеите имя персонажа"  # "Type the character name"
                 if tmsg.is_inline:
                     self.tAPI.edit(ms, tmsg.chat_id, None, tmsg.id)
                 else:
                     self.tAPI.send(ms, tmsg.chat_id, reply_to_message_id=tmsg.id)
         elif command.endswith('2'):
-            if len(command) > 1:
+            if len(query) > 1:
                 ms = "Этот персонаж(и) играет в {}:\n{}"  # "This(ese) character(s) play in {}:\n{}"
                 self.tAPI.send(ms.format(query,"\n".join(self.tAPI.db_shell.get_characters_of_movie(query.lower()))),
                                tmsg.chat_id, self.MENU_KEYBOARD, tmsg.id)
+                #self.tAPI.db_shell.add_user_history(tmsg.pers_id, command + " " + query, 55)
             else:
-                self.waitlist[(tmsg.pers_id, tmsg.chat_id)] = '2'
+                self.waitlist[(tmsg.pers_id, tmsg.chat_id)] = self.COMMAND + '2'
                 ms = "Введите название фильма" # "Type the movie title"
                 if tmsg.is_inline:
                     self.tAPI.edit(ms, tmsg.chat_id, None, tmsg.id)
                 else:
                     self.tAPI.send(ms, tmsg.chat_id, reply_to_message_id=tmsg.id)
         elif command.endswith('3'):
+            #self.tAPI.db_shell.add_user_history(tmsg.pers_id, command + " " + query, 55)
             ms = "Самый разыскиваемые фильмы:\n"  # "The most searched movies:\n"
             self.tAPI.send(ms + ("\n".join(self.tAPI.db_shell.get_most_searched_films())),
                            tmsg.chat_id, self.MENU_KEYBOARD, tmsg.id)
         elif command.endswith('4'):
+            #self.tAPI.db_shell.add_user_history(tmsg.pers_id, command + " " + query, 55)
             ms = "Ссылка на гистограмму годов производства фильмов"  # "Link to movies production years histogram:
             self.tAPI.send(ms + "\nhttp://nminec.ddns.net/years_hist",
                 tmsg.chat_id, self.MENU_KEYBOARD, tmsg.id)
         elif command.endswith('5'):
+            #self.tAPI.db_shell.add_user_history(tmsg.pers_id, command + " " + query, 55)
             ms = "Ссылка на кросстаблицу, которая содержит количество фильмов с одним годом производства и похожим рейтином"
                 # "Link to crosstable that contains the amount of movies with equal production year and similiar rating"
             self.tAPI.send(ms + ":\nhttp://nminec.ddns.net/years_hist",
                 tmsg.chat_id, self.MENU_KEYBOARD, tmsg.id)
+        elif command.endswith('6'):
+            #self.tAPI.db_shell.add_user_history(tmsg.pers_id, command + " " + query, 55)
+            ms = "Ваша история запросов:"
+            self.tAPI.send(ms + ("\n".join(self.tAPI.db_shell.get_user_history(tmsg.pers_id))),
+                tmsg.chat_id, self.MENU_KEYBOARD, tmsg.id)
+        return 0
+
+    def quit(self, pers_id, chat_id, additional_info = '', msg_id = 0):
+        if (pers_id, chat_id) in self.waitlist:
+            self.waitlist.pop((pers_id, chat_id))
+            if additional_info != '':
+                self.tAPI.send_inline_keyboard(additional_info, chat_id, self.MENU_KEYBOARD, msg_id)
+
+class Changer(BaseWorker):
+    COMMAND = "/change"
+    HELP = COMMAND + " - вызывает меню изменений\n\n"
+
+    waitlist = dict()
+
+    def is_it_for_me(self, tmsg):
+        return tmsg.text.startswith(self.COMMAND) or ((tmsg.pers_id, tmsg.chat_id) in self.waitlist)
+
+    def run(self, tmsg):
+        if (tmsg.pers_id == tmsg.chat_id):
+            self.tAPI.db_shell.update_last_activity(tmsg.pers_id, False)
+        if (tmsg.pers_id, tmsg.chat_id) in self.waitlist:
+            query = tmsg.text
+            command = self.waitlist[(tmsg.pers_id, tmsg.chat_id)]
+        else:
+            command = tmsg.text.split()
+            query = " ".join(command[1:])
+            command = command[0]
+        if command.endswith(self.COMMAND):
+            self.tAPI.send_inline_keyboard("Меню изменений", tmsg.chat_id,
+                                           [x for x in self.MENU_KEYBOARD if x['callback_data'].startswith(self.COMMAND)],
+                                           tmsg.id)
+        elif command.endswith('1'):
+            self.tAPI.db_shell.delete_user_history(tmsg.pers_id)
+            self.tAPI.send("Ваш запрос на удаление своих персональных данных удовлетоврен ",
+                tmsg.chat_id, self.MENU_KEYBOARD, tmsg.id)
+
+        elif command.endswith('2'):
+            if len(query) > 1:
+                self.tAPI.db_shell.delete_movie(query.lower())
+                #self.tAPI.db_shell.add_user_history(tmsg.pers_id, command+" "+query, 55)
+                self.tAPI.send_inline_keyboard("Несмотря на негодование пиратского сообщества фильм был удален",
+                               tmsg.chat_id, self.MENU_KEYBOARD, tmsg.id)
+            else:
+                self.waitlist[(tmsg.pers_id, tmsg.chat_id)] = self.COMMAND + '2'
+                ms = "Введите название фильма"  # "Type the movie title"
+                if tmsg.is_inline:
+                    self.tAPI.edit(ms, tmsg.chat_id, None, tmsg.id)
+                else:
+                    self.tAPI.send(ms, tmsg.chat_id, reply_to_message_id=tmsg.id)
+        elif command.endswith('3'):
+            if len(query) > 1:
+                self.tAPI.db_shell.delete_character(query.upper())
+                #self.tAPI.db_shell.add_user_history(tmsg.pers_id, command+" "+query, 55)
+                self.tAPI.send("Персонаж был наказан в суде и исключен из фильма",
+                               tmsg.chat_id, self.MENU_KEYBOARD, tmsg.id)
+            else:
+                self.waitlist[(tmsg.pers_id, tmsg.chat_id)] = self.COMMAND + '3'
+                ms = "Введеите имя персонажа"  # "Type the character name"
+                if tmsg.is_inline:
+                    self.tAPI.edit(ms, tmsg.chat_id, None, tmsg.id)
+                else:
+                    self.tAPI.send(ms, tmsg.chat_id, reply_to_message_id=tmsg.id)
         return 0
 
     def quit(self, pers_id, chat_id, additional_info = '', msg_id = 0):
@@ -188,9 +266,9 @@ class Info(BaseWorker):
 
     def run(self, tmsg):
         if tmsg.text.startswith("/start"):
-            self.tAPI.db_shell.initialize_user(tmsg.pers_id)
+            self.tAPI.db_shell.initialize_user(tmsg.pers_id, tmsg.username)
         if (tmsg.pers_id == tmsg.chat_id):
-            self.tAPI.db_shell.modify_last_activity(tmsg.pers_id, False)
+            self.tAPI.db_shell.update_last_activity(tmsg.pers_id, False)
         HELP = ""
         for worker in WorkersList.workers:
             HELP += worker[1].HELP
